b0130c940d32b26cff2ea895f3fb2a3fd60c9471
John Lavender <82977289+JohnLavender474@users.noreply.github.com>
2023-10-31 12:06:20 -0400

    Project 7: interactable cannons

--- Portfolio/Project 7/Assets/Cannon_Assets/Finished_Scene/Launch_with_Audio.cs
+++ Portfolio/Project 7/Assets/Cannon_Assets/Finished_Scene/Launch_with_Audio.cs
@@ -1,35 +1,87 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Random = UnityEngine.Random;
 
-public class Launch_with_Audio : MonoBehaviour
+namespace Cannon_Assets.Finished_Scene
 {
-
-    public GameObject projectile;
-    public float launchVelocity = 700f;
-    public AudioClip shootsound;
-    public float lowVolumeRange = .5f;
-    public float highVolumeRange = 1.0f;
-    private AudioSource source;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class LaunchWithAudio : MonoBehaviour
     {
-        source = GetComponent<AudioSource>();
-    }
+        public GameObject projectile;
+        public AudioClip shootSound;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
+        public float launchVelocity = 700f;
+        public float lowVolumeRange = .5f;
+        public float highVolumeRange = 1.0f;
+
+        private AudioSource source;
+        private MeshRenderer sphereRenderer;
+        private XRSimpleInteractable interactable;
+        
+        private bool inPlayerRay;
+        private bool selected;
+        private bool alreadyShot;
+
+        private void SetInPlayerRay(bool inRay)
         {
-            float vol = Random.Range(lowVolumeRange, highVolumeRange);
-            source.PlayOneShot(shootsound, vol);
+            if (alreadyShot)
+            {
+                Debug.Log("LaunchWithAudio: already shot, nothing more to do");
+                return;
+            }
 
-            GameObject launchThis = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+            Debug.Log("LaunchWithAudio: Set in player ray = " + inRay);
+            inPlayerRay = inRay;
+            sphereRenderer.enabled = inRay;
+        }
+
+        private void SetSelected(bool select)
+        {
+            selected = select;
+            Debug.Log("LaunchWithAudio: set selected = " + select);
+        }
+
+        private void Start()
+        {
+            inPlayerRay = false;
+            alreadyShot = false;
+            source = GetComponent<AudioSource>();
+
+            var sphere = (from Transform child in transform
+                where
+                    child.gameObject.name.Equals("Sphere")
+                select child.gameObject).FirstOrDefault();
+            if (sphere == null)
+            {
+                throw new Exception("LaunchWithAudio: No sphere found");
+            }
+
+            Debug.Log("LaunchWithAudio: fetched on start the sphere = " + sphere);
+
+            sphereRenderer = sphere.GetComponent<MeshRenderer>();
+            interactable = sphere.GetComponent<XRSimpleInteractable>();
+            interactable.hoverEntered.AddListener(_ => SetInPlayerRay(true));
+            interactable.hoverExited.AddListener(_ => SetInPlayerRay(false));
+            interactable.selectEntered.AddListener(_ => SetSelected(true));
+            interactable.selectExited.AddListener(_ => SetSelected(false));
+            Debug.Log("LaunchWithAudio: added listeners to interactable = " + interactable);
+
+            SetInPlayerRay(false);
+        }
+
+        private void Update()
+        {
+            if (alreadyShot || !inPlayerRay || !selected) return;
+
+            var vol = Random.Range(lowVolumeRange, highVolumeRange);
+            source.PlayOneShot(shootSound, vol);
+
+            var launchThis = Instantiate(projectile, transform.position, transform.rotation);
             launchThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, launchVelocity, 0));
+
+            alreadyShot = true;
+            sphereRenderer.material.color = Color.red;
         }
     }
-
 }
